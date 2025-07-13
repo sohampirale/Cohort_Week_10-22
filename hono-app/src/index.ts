9ace104014e40efa9d69067076b16d76a9610015
Soham Sachin Pirale <162886548+sohampirale@users.noreply.github.com>
2025-07-13 06:03:56 +0000

    fix  GET : / inside hono-app

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,9 +2,12 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.use('*',async (c,next)=>{
+  console.log('inside global middleware');
+  await next();
+})
+
 app.get('/', async (c) => {
-  const body = await c.req.json();
-  console.log(body);
   console.log(c.req.header("Authorization"));
   console.log(c.req.query("param"));
   

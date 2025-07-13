f1a787a0338ef2ee789d59a582f4bd2950712381
Soham Sachin Pirale <162886548+sohampirale@users.noreply.github.com>
2025-07-13 05:46:57 +0000

    created simple project with hono

---
+++ hono-app/src/index.ts
@@ -0,0 +1,16 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  
+  return c.json({
+    message:"Hello from GET /"
+  })
+})
+
+export default app

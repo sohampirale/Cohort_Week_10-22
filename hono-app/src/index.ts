de52c740218d3aaf89e44fb9c8b2a72b804244f3
Soham Sachin Pirale <162886548+sohampirale@users.noreply.github.com>
2025-07-13 05:54:25 +0000

    Wrotre post endpoitn in hono-app

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -13,4 +13,18 @@ app.get('/', async (c) => {
   })
 })
 
+
+app.post('/', async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  
+  return c.json({
+    message:"Hello from POST /"
+  })
+})
+
+
+
 export default app
